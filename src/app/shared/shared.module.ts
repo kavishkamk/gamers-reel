02dcd061366fbef2bfabe8a07897302d87094a94
kavishkamk <kavishkamadhushan727@gmail.com>
2023-09-22 19:11:37 +0530

    add login registration forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModelComponent } from './model/model.component';
-import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { ModelComponent } from './model/model.component';
+import { InputComponent } from './input/input.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
   declarations: [
     ModelComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective, 
+    NgxMaskPipe
   ],
   exports: [
     ModelComponent,
     TabComponent,
-    TabsContainerComponent
+    TabsContainerComponent,
+    InputComponent,
+    AlertComponent
   ],
+  providers: [provideNgxMask()]
 })
 export class SharedModule { }
